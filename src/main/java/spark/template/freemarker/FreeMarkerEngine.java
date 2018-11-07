befc8f60876ba388180cecfa954d834d4e83f64c
Florian Radlberger-Freude <florian.radlberger@gmail.com>
2018-11-07 23:33:32 +0100

    Maybe one of these days I'll manage to get it to run

--- src/main/java/spark/template/freemarker/FreeMarkerEngine.java
+++ src/main/java/spark/template/freemarker/FreeMarkerEngine.java
@@ -86,7 +86,7 @@ public class FreeMarkerEngine extends TemplateEngine {
 
     private Configuration createDefaultConfiguration() {
         Configuration configuration = new Configuration(new Version(2, 3, 23));
-        configuration.setClassForTemplateLoading(FreeMarkerEngine.class, "");
+        configuration.setClassForTemplateLoading(FreeMarkerEngine.class, "/templates/");
         return configuration;
     }
 
